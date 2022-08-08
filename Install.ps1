6618867fa54d6fd714aa5cd7b660f37582c6662d
felipegodias <felipe.g.o.dias@outlook.com>
2022-08-08 06:30:27 +0100

    Now supporting yaml files instead json;

--- Install.ps1
+++ Install.ps1
@@ -1,4 +1,10 @@
-#Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+$MinRequiredPowershellVersion = [System.Version]"7.0.0"
+$PowershellVersion = $PSVersionTable.PSVersion
+
+if ($PowershellVersion -lt $MinRequiredPowershellVersion) {
+    Write-Error "Instalation requires Powershell to be at least at version '$MinRequiredPowershellVersion'!"
+    exit(1)
+}
 
 Write-Host "Instaling OhMyPosh..."
 winget install JanDeDobbeleer.OhMyPosh --accept-package-agreements --accept-source-agreements
@@ -12,9 +18,12 @@ Install-Module -Name posh-git -AllowClobber -Scope CurrentUser -Force
 Write-Host "Instaling Get-ChildItemColor..."
 Install-Module -Name Get-ChildItemColor -AllowClobber -Scope CurrentUser -Force
 
+Write-Host "Instaling powershell-yaml..."
+Install-Module -Name powershell-yaml -AllowClobber -Scope CurrentUser -Force
+
 $CurrentLocation = Get-Location
 $CurrentLocation = $CurrentLocation.path
 
 New-Item -Path $profile -ItemType File -Force
 Add-Content -Path $profile -Value "Import-Module $CurrentLocation"
-Add-Content -Path $profile -Value "Invoke-Main $CurrentLocation/dummy.json"
+Add-Content -Path $profile -Value "Invoke-Main $CurrentLocation/dummy.yaml"
--- PowerShellProfile.psm1
+++ PowerShellProfile.psm1
@@ -1,16 +1,14 @@
 $VS_WHERE = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
 $POSH_THEME = ".mytheme.omp.json"
 
-function Set-ListFilesAliases
-{
+function Set-ListFilesAliases {
     Import-Module Get-ChildItemColor -Global
         
     Set-Alias -Name l -Value Get-ChildItem -Option AllScope -Scope Global
     Set-Alias -Name ls -Value Get-ChildItemColorFormatWide -Option AllScope -Scope Global
 }
 
-function Set-NavigableMenu
-{
+function Set-NavigableMenu {
     # Shows navigable menu of all options when hitting Tab
     Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
@@ -19,8 +17,7 @@ function Set-NavigableMenu
     Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 }
 
-function Start-Cmd($cmd, $arguments)
-{
+function Start-Cmd($cmd, $arguments) {
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     $pinfo.FileName = $cmd
     $pinfo.RedirectStandardError = $true
@@ -34,124 +31,122 @@ function Start-Cmd($cmd, $arguments)
     return $p.StandardOutput.ReadToEnd()
 }
 
-function Enter-DevShell
-{
+function Enter-DevShell {
     $VsWhereExists = Test-Path -Path $VS_WHERE -PathType Leaf
-    if (-NOT $VsWhereExists)
-    {
+    if (-NOT $VsWhereExists) {
         return
     }
 
     $vsInstanceId = Start-Cmd $VS_WHERE "-latest -property instanceId"
     $vsInstallPath = Start-Cmd $VS_WHERE "-latest -property installationPath"
 
-    $vsInstanceId = $vsInstanceId -replace "\r\n",""
-    $vsInstallPath = $vsInstallPath -replace "\r\n",""
+    $vsInstanceId = $vsInstanceId -replace "\r\n", ""
+    $vsInstallPath = $vsInstallPath -replace "\r\n", ""
     
     Import-Module "$vsInstallPath\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
     Enter-VsDevShell -VsInstanceId $vsInstanceId -SkipAutomaticLocation -DevCmdArguments '-arch=x64 -no_logo'
 }
 
-function Set-PersonalAliases
-{
-    Set-Alias -Name slp -Value Set-LocationToProject -Scope Global
-    Set-Alias -Name ia -Value Invoke-Action -Scope Global
+function Set-PersonalAliases {
+    Set-Alias -Name i -Value Invoke-Pipeline -Scope Global
     Set-Alias -Name open -Value Invoke-Open -Scope Global
 }
 
-function Invoke-Open([string] $Path)
-{
-    if ($Path -eq "")
-    {
+function Invoke-Open([string] $Path) {
+    if ($Path -eq "") {
         explorer .
     }
-    else
-    {
+    else {
         Invoke-Item $Path
     }
 }
 Export-ModuleMember Invoke-Open
 
-function Set-LocationToProject
-{
+function Set-LocationOrThrow {
     param(
-        $Project
+        $Path
+    ) 
+
+    $PathExists = Test-Path -Path $Path
+    if (-NOT $PathExists) {
+        throw "Cannot find path '$Path' because it does not exist."
+    }
+
+    $CurrentLocation = Set-Location -PassThru $Path
+    if (-NOT $CurrentLocation) {
+        throw
+    }
+}
+
+function Invoke-PipelineStep {
+    param(
+        $PipelineStep
+    )
+
+    $CurrentWorkingDirectory = Get-Location
+    $IsChangingWorkingDiretory = $PipelineStep.Contains("working_directory")
+    try {
+        if ($IsChangingWorkingDiretory) {
+            Set-LocationOrThrow $PipelineStep.working_directory
+        }
+
+        Invoke-Expression $PipelineStep.command
+        $ExpressionExitCode = $LASTEXITCODE
+        if ($ExpressionExitCode -NE 0) {
+            throw "Pipeline step failed with exit code '$ExpressionExitCode'!"
+        }
+    }
+    finally {
+        if ($IsChangingWorkingDiretory) {
+            Set-Location $CurrentWorkingDirectory
+        }
+    }
+}
+
+function Invoke-Pipeline {
+    param(
+        $PipelineName
     )
 
     $ProfileContent = Get-Content $Global:InProfile
-    $SelectedProfile = $ProfileContent | ConvertFrom-Json
+    $SelectedProfile = $ProfileContent | ConvertFrom-Yaml
+    $Pipelines = $SelectedProfile.pipelines
 
-    $Projects = $SelectedProfile.projects
-    $ProjectExists = $null -NE $Project -AND $Projects.PSObject.Properties.Item($Project)
-    if (-NOT $ProjectExists)
-    {
-        Write-Host "Project '$Project' does not exists in the profile! Do you mean one of these?"
+    $PipelineExists = $null -NE $PipelineName -AND $Pipelines.Contains($PipelineName)
+    if (-NOT $PipelineExists) {
+        Write-Host "The Pipeline '$PipelineName' does not exists in the profile! Do you mean one of these?"
+
         $Table = @()
-        foreach ($Key in $Projects.PSObject.Properties) {
-            $KeyName = $Key.Name
-            $ProjectName = $Projects.$KeyName.name
-            $Row = "" | Select-Object Alias,Name
-            $Row.Alias = $KeyName
-            $Row.Name = $ProjectName
+        foreach ($PipelineName in $Pipelines.Keys | Sort-Object) {
+            $Row = "" | Select-Object Pipeline, Description
+            $Row.Pipeline = $PipelineName
+            $Row.Description = $Pipelines.$PipelineName.description
             $Table += $Row
         }
         $Table
         return
     }
 
-    $Location = $Projects.$Project.path
-    Write-Host "Going to: '$Location'"
-    Set-Location $Location
-}
-Export-ModuleMember Set-LocationToProject
-
-function Invoke-Action
-{
-    param(
-        $Action
-    )
-
-    $ProfileContent = Get-Content $Global:InProfile
-    $SelectedProfile = $ProfileContent | ConvertFrom-Json
-
-    $Actions = $SelectedProfile.actions
+    $CurrentWorkingDirectory = Get-Location
+    $IsChangingWorkingDiretory = $Pipelines.$PipelineName.Contains("working_directory")
+    try {
+        if ($IsChangingWorkingDiretory) {
+            Set-LocationOrThrow $Pipelines.$PipelineName.working_directory
+        }
     
-    $ActionsExists = $null -NE $Action -AND $Actions.PSObject.Properties.Item($Action)
-    if (-NOT $ActionsExists)
-    {
-        Write-Host "Action '$Action' does not exists in the profile! Do you mean one of these?"
-        $Table = @()
-        foreach ($Key in $Actions.PSObject.Properties) {
-            $KeyName = $Key.Name
-            $ActionDescription = $Actions.$KeyName.description
-            $Row = "" | Select-Object Action,Description
-            $Row.Action = $KeyName
-            $Row.Description = $ActionDescription
-            $Table += $Row
+        foreach ($PipelineStep in  $Pipelines.$PipelineName.steps) {
+            Invoke-PipelineStep $PipelineStep
         }
-        $Table
-        return
     }
-
-    $Cmd = $Actions.$Action.cmd
-    $WdExists = $Actions.$Action.PSObject.Properties.Item("wd")
-    $Cwd = Get-Location
-    if ($WdExists)
-    {
-        Set-Location $Actions.$Action.wd
-    }
-
-    Invoke-Expression $Cmd
-
-    if ($WdExists)
-    {
-        Set-Location $Cwd
+    finally {
+        if ($IsChangingWorkingDiretory) {
+            Set-Location $CurrentWorkingDirectory
+        }
     }
 }
-Export-ModuleMember Invoke-Action
+Export-ModuleMember Invoke-Pipeline
 
-function Invoke-Main
-{
+function Invoke-Main {
     param(
         $InProfile
     )
